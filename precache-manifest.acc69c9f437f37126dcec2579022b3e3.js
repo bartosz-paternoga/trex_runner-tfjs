58a0e01e5334cd32d7d12ae82021969c38413792
bartosz-paternoga <bpaternoga@hotmail.com>
2019-01-01 18:17:29 +0100

    Updates

---
+++ precache-manifest.acc69c9f437f37126dcec2579022b3e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8e05b0fc4c348cb8a5c",
+    "url": "/trex_runner-tfjs/static/css/main.33449ae1.chunk.css"
+  },
+  {
+    "revision": "f8e05b0fc4c348cb8a5c",
+    "url": "/trex_runner-tfjs/static/js/main.f8e05b0f.chunk.js"
+  },
+  {
+    "revision": "60c798ca4a6c2850e98b",
+    "url": "/trex_runner-tfjs/static/js/1.60c798ca.chunk.js"
+  },
+  {
+    "revision": "0d6a93baced2a997589e",
+    "url": "/trex_runner-tfjs/static/js/runtime~main.0d6a93ba.js"
+  },
+  {
+    "revision": "cc18b3523694f3522ac5dfa06aef2412",
+    "url": "/trex_runner-tfjs/index.html"
+  }
+];
